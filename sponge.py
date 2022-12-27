77bd7b26681c064869088533569d299937f82f33
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-12-26 21:10:21 -0700

    Create sponge.py
    outputs raw html from daily blast

---
+++ sponge.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# set up the request parameters
+params = {
+'api_key': '54737D4576E3465B9A02285845AD6587',
+  'q': 'pizza',
+  'include_html': 'true',
+  'output': 'html'
+}
+
+# make the http GET request to Scale SERP
+api_result = requests.get('https://webapps.mines.edu/DailyBlast/Home/PerformSearch', params)
+
+# print the HTML response from Scale SERP
+print(api_result.content)
