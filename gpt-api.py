3df741059748783a4aeef699ea4daf8e45574472
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-12-28 22:03:22 -0700

    api errors
    goodnight!

--- gpt-api.py
+++ gpt-api.py
@@ -1,4 +1,4 @@
-import requests
+"""import requests
 
 # Set the API endpoint and your API key
 api_endpoint = "https://api.openai.com/v1/text-davinci/completions"
@@ -35,6 +35,36 @@ response = requests.post(
         "completion_options": completion_options,
     }
 )
+"""
 
+import openai
+
+openai.api_key = "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"
+
+#TODO change to cleanBlast.txt when finished testing
+read_path = "sampleDailyBlast-11-10-22.txt"
+"""
+#reading in and setting the blastPrompt
+with open(read_path, "r") as f:
+    # Read the contents of the file as a string
+    blastPrompt = f.read()
+
+initialPrompt = "Are there any events with free food, breakfast, lunch, dinner, or drinks in the following email? If so, when and where are they? "
+"""
+#final_prompt = initialPrompt + blastPrompt
+final_prompt = "Write a 2 paragraph essay about sponges"
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=final_prompt,
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
 # Print the generated text
-print(response.json()["completions"][0]["text"])
+##   completions = response.json()["completions"]
+#except KeyError:
+#    print("The 'completions' key was not found in the response")
+print(response.text)
