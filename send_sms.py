2cd17f754b187dd32260ab78b4a54cf9cbe91b75
fredricksonP <112718203+fredricksonP@users.noreply.github.com>
2023-01-01 21:24:14 -0700

    Cock and ball torture

--- send_sms.py
+++ send_sms.py
@@ -6,10 +6,10 @@ account_sid = "AC54b1eccfa25ab3d29ea309bbc9c50448"
 auth_token  = "ebc4de3f9adf4794efe5722c8f7ea977"
 
 client = Client(account_sid, auth_token)
-numbers = ["720 383 3726", "719 985 5619"]
+numbers = ["720 383 3726", "719 985 5619", "9709787468"]
 for number in numbers:
   message = client.messages.create(
       to=number,
       from_="+13148873334",
-      body="Hurry the fuck up")
+      body="Fuck your meet zachery")
   print(message.sid)
