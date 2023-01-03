579c14fbe5741a3907a1f4dbf3d44b2e3d5b76f4
zcrennen <98796321+zcrennen@users.noreply.github.com>
2023-01-02 21:09:47 -0700

    Added scraping, parsing, and prompting to eventgpt

--- event-gpt.py
+++ event-gpt.py
@@ -2,9 +2,54 @@
     #input: cleanLinks.csv
     #output: finalSMS.txt
 
-links = []
+import openai
+import requests
+from bs4 import BeautifulSoup
+openai.api_key = "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"
+scaleSerpKey = "54737D4576E3465B9A02285845AD6587"
 
-with open("cleanLinks.csv", "r") as cl:
+links = []
+prompt = "Name the type of food, drink, breakfast, lunch or dinner in the following event, as well as where and when it is. Use this format [event name, time, location, type of food/drink] "
+
+readpath = "sampleCleanLinks.csv"
+
+with open(readpath, "r") as cl:
     for line in cl:
         links.append(line.strip())
+
+for link in links:
+#scrape
+    # set up the request parameters
+    params = {
+        'api_key': scaleSerpKey,
+        'q': 'pizza',
+        'include_html': 'true',
+        'output': 'html'
+        }
+
+    # make the http GET request to Scale SERP
+    api_result = requests.get(link, params)
+    #print(api_result.content)
+
+#parse
+    soup = BeautifulSoup(api_result.content, 'html.parser')
+    rawSoup = soup.get_text()
+    ingredients = rawSoup.split('Campus Announcements')
+    cookedSoup = ingredients[1]
+    print(cookedSoup)
+    prompt += cookedSoup
+
+#prompt
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    event_string = response['choices'][0]['text']
+    print(event_string)
 
