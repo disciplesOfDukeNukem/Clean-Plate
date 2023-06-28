87ce4e3ba0d36ab3a23ab91b97046ad29b1de556
Zach Crennen <98796321+zcrennen@users.noreply.github.com>
2023-06-28 17:03:32 -0600

    Update twilioTimerSMS.py
    removed personal info and api keys

--- twilioTimerSMS.py
+++ twilioTimerSMS.py
@@ -3,18 +3,18 @@ import schedule
 import time
 
 # Your Account SID from twilio.com/console
-account_sid = "AC54b1eccfa25ab3d29ea309bbc9c50448"
+account_sid = "INSERT_ACCOUNT_SID"
 # Your Auth Token from twilio.com/console
-auth_token  = "ebc4de3f9adf4794efe5722c8f7ea977"
+auth_token  = "INSERT_AUTH_TOKEN"
 
 client = Client(account_sid, auth_token)
-numbers = ["720 383 3726", "719 985 5619", "970 978 7468"]
+numbers = ["INSERT_PHONE_NUMBER", "INSERT_PHONE_NUMBER"]
 
 def show_name():
     for number in numbers:
         message = client.messages.create(
         to=number,
-        from_="+13148873334",
+        from_="INSERT_PHONE_NUMBER",
         body="This is testing Jupyter (1 text per minute)! Free the Tate brothers")
         print(message.sid)
     print("This is working on a 1 min schedule")
